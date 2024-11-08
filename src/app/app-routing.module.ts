6ed274a00c8a24b53b2400f6a4a8f0823d09d114
fishahszd <fisha@fisha.hu>
2024-11-08 10:34:57 +0100

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { inject, NgModule } from '@angular/core';
+import { mapToCanActivate, Router, RouterModule, Routes } from '@angular/router';
+import { ShoppingListListComponent } from './shopping-list-list/shopping-list-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { LoginComponent } from './login/login.component';
+import { AuthService } from './auth.service';
+import { authGuard } from './auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'lists/:id', 
+    component: ShoppingListComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'lists', 
+    component: ShoppingListListComponent,
+    canActivate: [authGuard]
+  },
+  {path: '', redirectTo: '/lists', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Subject, Subscription } from 'rxjs';
+import { User } from './user.model';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'shopping-list';
+  loggedIn = false;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private auth: AuthService) {
+    this.loggedIn = this.auth.getUser() != null;
+  }
+  
+  ngOnInit(): void {
+    console.log("apiurl:" + environment.apiUrl);
+    this.subscriptions.push(this.auth.loginSuccess.subscribe({
+      next: (user) => {
+        this.loggedIn = true;
+      }
+    }));
+
+    this.subscriptions.push(this.auth.logoutSuccess.subscribe({
+      next: () => {
+        this.loggedIn = false;
+      }
+    }))
+  }
+  
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
+  }
+
+  logout() {
+    console.log("logout");
+    this.auth.logout();
+  }
 }
